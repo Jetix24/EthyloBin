9768b8d20c761b7724c87a9bb74211b19f449bd2
solofervqz <solofervazquez@gmail.com>
2024-06-14 00:11:41 -0600

    switch to typescript

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import ButtonSignin from "@/components/ButtonSignin";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      Hola
+    </div>
+  );
+}
