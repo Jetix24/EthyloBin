4a44df1243faa95653a86bbbdcc3b4311bd02cb6
Jetix24 <josepabloaguirre02@gmail.com>
2024-06-18 01:08:30 -0600

    ADD: Landing page CTA

--- components/CTA.js
+++ components/CTA.js
@@ -12,7 +12,7 @@ const CTA = () => {
       />
       <div className="relative hero-overlay bg-blue_500 bg-opacity-60"></div>
       <div className="relative hero-content text-center text-neutral-content p-8 w-full">
-        <div className="flex flex-col items-center max-w-xl p-8 md:p-0 ">
+        <div className="flex flex-col items-center max-w-4xl p-8 md:p-0 ">
           <h2 className="font-bold text-3xl md:text-5xl tracking-tight mb-8 md:mb-12">
           Simplifica la gestión de inventario para tu negocio de alimentos
           </h2>
@@ -20,8 +20,8 @@ const CTA = () => {
           Optimiza tu tiempo y aumenta la eficiencia con nuestra herramienta digital fácil de usar
           </p>
 
-          <button className="btn bg-blue_200 rounded-md border-0 hover:bg-blue_100 btn-wide ">
-            Get {config.appName}
+          <button className="btn bg-blue_200 rounded-md text-lg text-purple_200 border-0 hover:bg-blue_100 btn-wide ">
+            Empieza ahora
           </button>
         </div>
       </div>
