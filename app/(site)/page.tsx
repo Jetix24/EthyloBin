eb4834b34562219f7054213f30a31032469c058c
solofervqz <solofervazquez@gmail.com>
2024-06-17 18:47:54 -0600

    Get back landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,11 +1,28 @@
-import Link from "next/link";
-import ButtonSignin from "@/components/ButtonSignin";
-import React from "react";
+import { Suspense } from 'react'
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Problem from "@/components/Problem";
+import FeaturesAccordion from "@/components/FeaturesAccordion";
+import Pricing from "@/components/Pricing";
+import FAQ from "@/components/FAQ";
+import CTA from "@/components/CTA";
+import Footer from "@/components/Footer";
 
-export default function Page() {
+export default function Home() {
   return (
-    <div>
-      Hola
-    </div>
+    <>
+      <Suspense>
+        <Header />
+      </Suspense>
+      <main>
+        <Hero />
+        <Problem />
+        <FeaturesAccordion />
+        <Pricing />
+        <FAQ />
+        <CTA />
+      </main>
+      <Footer />
+    </>
   );
 }
