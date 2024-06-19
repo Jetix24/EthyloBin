4e5dbf896afeaa59a1169d34144f5d8965f0d507
solofervqz <solofervazquez@gmail.com>
2024-06-19 02:08:24 -0600

    Add icons in desktop sidebar

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -1,38 +1,36 @@
 import { useMemo } from "react";
 import { usePathname } from "next/navigation";
-import { HiChat } from "react-icons/hi";
-import {
-  HiArrowLeftOnRectangle,
-  HiUsers
-} from "react-icons/hi2";
-import { signOut } from "next-auth/react";
-
+import { FaBasketShopping } from "react-icons/fa6";
+import {  HiUsers } from 'react-icons/hi2';
+import { GiFruitBowl } from "react-icons/gi";
+import { MdOutlineKitchen } from "react-icons/md";
 
 const useRoutes = () => {
   const pathname = usePathname();
 
   const routes = useMemo(() => [
     {
-      label: 'Chat',
-      href: '/conversations',
-      icon: HiChat,
-      active: pathname === '/conversations' 
+      label: 'Zonas', 
+      href: '/alacenas',
+      icon: MdOutlineKitchen, 
+      active: pathname === '/alacenas'
     },
-    {
-      label: 'Users',
-      href: '/users',
-      icon: HiUsers,
-      active: pathname === '/users'
+    { 
+      label: 'Materia Prima', 
+      href: '/materiaprima', 
+      icon: GiFruitBowl,
+      active: pathname === '/materiaprima'
     },
-    {
-      label: 'Logout',
-      href: '#',
-      onClick: () => signOut(),
-      icon: HiArrowLeftOnRectangle
+    { 
+      label: 'Proveedores', 
+      href: '/proveedores', 
+      icon: FaBasketShopping, 
+      active: pathname === '/proveedores'
     }
+    
   ], [pathname]);
 
   return routes;
-}
+};
 
 export default useRoutes;
--- components/sidebar/DesktopSidebar.tsx
+++ components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 import useRoutes from "@/app/hooks/useRoutes";
+import Image from "next/image";
 
 interface DesktopSidebarProps {
   currentUserImage: string;
@@ -25,74 +26,92 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
         onClose={() => setIsOpen(false)}
       /> */}
       <div
-        className="
-          hidden
-          lg:fixed
-          lg:inset-y-0
-          lg:left-0
-          lg:z-40
-          lg:w-20
-          xl:px-6
-          lg:overflow-y-auto
-          lg:bg-white
-          lg:border-r-[1px]
-          lg:pb-4
-          lg:flex
-          lg:flex-col
-          justify-between
-        "
-      >
-        <nav
           className="
-            mt-4
-            flex
-            flex-col
-            justify-between
-          "
-        >
-          <ul
-            role="list"
-            className="
-              flex
-              flex-col
-              items-center
-              space-y-1
-            "
-          >
-            {routes.map((item) => (
-              <DesktopItem
-                key={item.label}
-                href={item.href}
-                label={item.label}
-                icon={item.icon}
-                active={item.active}
-                onClick={item.onClick}
-              />
-            ))}
-          </ul>
-        </nav>
-        <nav
-          className="
-            mt-4
-            flex
-            flex-col
-            justify-between
-            items-center
+            hidden
+            lg:fixed
+            lg:inset-y-0
+            lg:left-0
+            lg:z-40
+            lg:w-20
+            lg:overflow-y-auto
+            lg:bg-slate-900
+            lg:border-r-[1px]
+            border-slate-800
+            lg:pb-4
+            lg:flex
+            lg:flex-col
           "
         >
           <div
-            onClick={() => setIsOpen(true)}
             className="
-              cursor-pointer
-              hover:opacity-75
-              transition
+            py-4
+            flex
+            items-center
+            justify-center   
+            w-full
+            h-50        
             "
           >
-            <Avatar image={currentUserImage} />
+            <Image
+              src="/img/icon.png"
+              alt="Logo"
+              width={50}
+              height={50}
+            />
           </div>
-        </nav>
-      </div>
-    </>
+          <hr className="w-full border-slate-600" />
+          <nav
+            className="
+              mt-4
+              flex
+              flex-col
+              justify-between
+            "
+          >
+            <ul
+              role="list"
+              className="
+                flex
+                flex-col
+                items-center
+                space-y-1
+              "
+            >
+              {routes.map((item) => (
+                <DesktopItem
+                  key={item.label}
+                  href={item.href}
+                  label={item.label}
+                  icon={item.icon}
+                  active={item.active}
+                />
+              ))}
+            </ul>
+          </nav>
+          <nav
+            className="
+              mt-4
+              flex
+              flex-col
+              justify-between
+              items-center
+              h-96
+            "
+          >
+            <div
+              onClick={() => setIsOpen(true)}
+              className="
+                cursor-pointer
+                hover:opacity-75
+                transition
+                mt-auto
+              "
+            >
+              <Avatar image={currentUserImage} />
+            </div>
+          </nav>
+        </div>
+      </>
    );
 }
  
