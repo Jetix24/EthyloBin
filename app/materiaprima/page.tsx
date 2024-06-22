d505265f268e2261e6e1a8518e26aa08177d9bf7
solofervqz <solofervazquez@gmail.com>
2024-06-21 21:04:33 -0600

    Fix EmptyState title color

--- app/materiaprima/page.tsx
+++ app/materiaprima/page.tsx
@@ -4,17 +4,13 @@ import EmptyState from "@/components/EmptyState";
 import clsx from "clsx";
 
 const Home = () => {
-
   return (
     <div
-      className={clsx(
-        "lg:pl-80 h-full lg:block",
-        true ? 'block' : 'hidden'
-      )}
+      className={clsx("lg:pl-80 h-full lg:block", true ? "block" : "hidden")}
     >
       <EmptyState />
     </div>
-  )
+  );
 };
 
 export default Home;
