a1e1f721e10ca194e34158501c543fdb44d3112c
solofervqz <solofervazquez@gmail.com>
2024-06-22 02:04:27 -0600

    Add Agregar route in useRoutes

--- app/hooks/useRoutes.ts
+++ app/hooks/useRoutes.ts
@@ -3,31 +3,40 @@ import { usePathname } from "next/navigation";
 import { FaBasketShopping } from "react-icons/fa6";
 import { GiFruitBowl } from "react-icons/gi";
 import { MdOutlineKitchen } from "react-icons/md";
+import { IoMdAddCircle } from "react-icons/io";
 
 const useRoutes = () => {
   const pathname = usePathname();
 
-  const routes = useMemo(() => [
-    {
-      label: 'Zonas', 
-      href: '/zonas',
-      icon: MdOutlineKitchen, 
-      active: pathname === '/zonas'
-    },
-    { 
-      label: 'Materia Prima', 
-      href: '/materiaprima', 
-      icon: GiFruitBowl,
-      active: pathname === '/materiaprima'
-    },
-    { 
-      label: 'Proveedores', 
-      href: '/proveedores', 
-      icon: FaBasketShopping, 
-      active: pathname === '/proveedores'
-    }
-    
-  ], [pathname]);
+  const routes = useMemo(
+    () => [
+      {
+        label: "Agregar",
+        href: "/materiaprima",
+        icon: IoMdAddCircle,
+        active: pathname === "/materiaprima",
+      },
+      {
+        label: "Zonas",
+        href: "/zonas",
+        icon: MdOutlineKitchen,
+        active: pathname === "/zonas",
+      },
+      {
+        label: "Categorías",
+        href: "/categorias",
+        icon: GiFruitBowl,
+        active: pathname === "/categorias",
+      },
+      {
+        label: "Proveedores",
+        href: "/proveedores",
+        icon: FaBasketShopping,
+        active: pathname === "/proveedores",
+      },
+    ],
+    [pathname]
+  );
 
   return routes;
 };
