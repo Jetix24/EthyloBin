3276fe95a46c9d299c90e7c054057c4efac06b4b
solofervqz <solofervazquez@gmail.com>
2024-06-27 01:02:02 -0600

    Fix CSS but ERROR in quantity update functionality MateriaZoneBox

--- app/zonas/[id]/components/MateriaZonaBox.js
+++ app/zonas/[id]/components/MateriaZonaBox.js
@@ -7,7 +7,7 @@ const MateriaZonaBox = ({ materia, onQuantityChange }) => {
   const handleQuantityChange = async (newQuantity) => {
     setQuantity(newQuantity);
     try {
-      await apiClient.put(`/materias-primas/${materia._id}/cantidad`, {
+      await apiClient.put(`/api/materias-primas/${materia._id}/cantidad`, {
         cantidad: newQuantity,
       });
       onQuantityChange(materia._id, newQuantity);
@@ -56,11 +56,12 @@ const MateriaZonaBox = ({ materia, onQuantityChange }) => {
             </svg>
           </button>
           <input
-            type="number"
+            type="text"
             id="quantity-input"
+            data-input-counter
             value={quantity}
             onChange={(e) => handleQuantityChange(Number(e.target.value))}
-            className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5"
+            className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 appearance-none"
             min="0"
           />
           <button
