41dfb85ad62ef98778860edc3da2ded4431af86b
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-04 19:58:46 -0600

    Update: fix layout general

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,18 +3,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 import config from "@/config";
 import Sidebar from "@/components/sidebar/Sidebar";
+import getCurrentUser from "@/app/actions/getCurrentUser";
 
-// This is a server-side component to ensure the user is logged in.
-// If not, it will redirect to the login page.
-// It's applied to all subpages of /dashboard in /app/dashboard/*** pages
-// You can also add custom static UI elements like a Navbar, Sidebar, Footer, etc..
-// See https://shipfa.st/docs/tutorials/private-page
 export default async function LayoutPrivate({ children }) {
   const session = await getServerSession(authOptions);
+  const user = await getCurrentUser();
 
-  //If not session, redirect to login page
   if (!session) redirect(config.auth.loginUrl);
-  //if (!session.hasAccess) redirect(config.auth.loginUrl);
+  if (!user.hasAccess) redirect(config.auth.loginUrl);
 
   return (
     <Sidebar>
