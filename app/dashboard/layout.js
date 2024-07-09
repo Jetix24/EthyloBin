c4ad7096fce7f87a9ed24b4c3773b5488a01a874
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-09 17:59:36 -0600

    Update: flujo hasAccess por mientras

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -6,16 +6,16 @@ import Sidebar from "@/components/sidebar/Sidebar";
 import getCurrentUser from "../actions/getCurrentUser";
 
 export default async function LayoutPrivate({ children }) {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
   
-  if (!session) 
-    redirect(config.auth.loginUrl);
+  // if (!session) 
+  //   redirect(config.auth.loginUrl);
 
-  const user = await getCurrentUser(session)
-
-  if(user?.hasAccess === false){
-    redirect(config.auth.landUrlPri);
-  }
+  // const user = await getCurrentUser(session)
+  // console.log('user',user)
+  // if(user?.hasAccess === false || user?.hasAccess === undefined){
+  //   redirect(config.auth.landUrlPri);
+  // }
 
   return (
     <Sidebar>
