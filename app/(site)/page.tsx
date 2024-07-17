bff48a86354d4377fa2e758fbdb63fdd28f19c33
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-16 23:15:44 -0600

    Add: Problem landing page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,8 +3,12 @@ import Header from "@/components/landingPage/Header";
 import Characteristics from "@/components/landingPage/Characteristics";
 import Pricing from "@/components/landingPage/Pricing";
 import CTA from "@/components/landingPage/CTA";
-import Footer from "@/components/Footer";
+import Footer from "@/components/landingPage/Footer";
 import CarouselText from '@/components/landingPage/CarouselText';
+import FAQ from '@/components/FAQ';
+import Problem from '@/components/landingPage/Problem';
+import FeaturesAccordion from '@/components/FeaturesAccordion';
+import FeaturesListicle from '@/components/FeaturesListicle';
 
 export default async function Home() {
 
@@ -15,9 +19,12 @@ export default async function Home() {
       </Suspense>
       <main>
         <CTA/>
+        <Problem />
         <Characteristics/>
-        <CarouselText/>
-        <Pricing/>      
+        <CarouselText/> 
+        <FeaturesAccordion/> 
+        <Pricing/> 
+        <FAQ/>     
       </main>
       <Footer />
     </>
