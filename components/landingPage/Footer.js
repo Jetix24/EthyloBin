3a8acf47dd2b290b974d1c5bc7d3aa1b891924b9
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-16 23:23:45 -0600

    Update: Colors footer

--- components/landingPage/Footer.js
+++ components/landingPage/Footer.js
@@ -8,7 +8,7 @@ import logo from "@/app/icon.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-base-200 border-t border-base-content/10">
+    <footer className="bg-dark_purple border-t border-base-content/10">
       <div className="max-w-7xl mx-auto px-8 py-24">
         <div className=" flex lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
@@ -25,25 +25,25 @@ const Footer = () => {
                 width={24}
                 height={24}
               />
-              <strong className="font-extrabold tracking-tight text-base md:text-lg">
+              <strong className="font-extrabold tracking-tight text-cute_white md:text-lg">
                 {config.appName}
               </strong>
             </Link>
 
-            <p className="mt-3 text-sm text-base-content/80">
+            <p className="mt-3 text-sm text-white_purple">
               {config.appDescription}
             </p>
-            <p className="mt-3 text-sm text-base-content/60">
+            <p className="mt-3 text-sm text-white_purple/60">
               Copyright © {new Date().getFullYear()} - All rights reserved
             </p>
           </div>
           <div className="flex-grow flex flex-wrap justify-center -mb-10 md:mt-0 mt-10 text-center">
-            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-              <div className="footer-title font-semibold text-base-content tracking-widest text-sm md:text-left mb-3">
+            <div className="lg:w-1/3 md:w-1/3 w-full px-4">
+              <div className="font-semibold text-white_purple tracking-widest text-sm md:text-left mb-3">
                 LINKS
               </div>
 
-              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
+              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm text-white_purple/60">
                 {config.mailgun.supportEmail && (
                   <a
                     href={`mailto:${config.mailgun.supportEmail}`}
@@ -66,12 +66,12 @@ const Footer = () => {
               </div>
             </div>
 
-            <div className="lg:w-1/3 md:w-1/2 w-full px-4">
-              <div className="footer-title font-semibold text-base-content tracking-widest text-sm md:text-left mb-3">
+            <div className="lg:w-1/3 md:w-1/3 w-full px-4">
+              <div className="font-semibold text-white_purple tracking-widest text-sm md:text-left mb-3">
                 LEGAL
               </div>
 
-              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm">
+              <div className="flex flex-col justify-center items-center md:items-start gap-2 mb-10 text-sm text-white_purple/60">
                 <Link href="/tos" className="link link-hover">
                   Terms of services
                 </Link>
