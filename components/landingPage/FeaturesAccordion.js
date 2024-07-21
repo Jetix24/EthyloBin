f3281d350761e38fb414da5e1850418f4df407a9
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-20 20:01:16 -0600

    Update: video landing

--- components/landingPage/FeaturesAccordion.js
+++ components/landingPage/FeaturesAccordion.js
@@ -21,7 +21,7 @@ const features = [
     description:
       "Son las diferentes secciones de tu negocio, como la cocina, el bar, el salón, etc. Cada área puede tener diferentes productos.",
     type: "video",
-    path: "/img/Areas.mp4",
+    path: "/img/Areas.webm",
     format: "video/webm",
     icon: MdOutlineKitchen,
   },
@@ -30,7 +30,7 @@ const features = [
     description:
       "Son las diferentes clasificaciones de tus productos, como bebidas, alimentos, postres, etc. Cada producto puede tener una categoría.",
     type: "video",
-    path: "/img/Categorias.mp4",
+    path: "/img/Categorias.webm",
     alt: "video/webm",
     icon: GiFruitBowl,
   },
@@ -39,7 +39,7 @@ const features = [
     description:
       "Son las empresas o personas que te suministran los productos que vendes. Puedes registrar sus datos y los productos que te suministran.",
       type: "video",
-      path: "/img/Proveedor.mp4",
+      path: "/img/Proveedor.webm",
       alt: "video/webm",
     icon: FaBasketShopping,
   },
