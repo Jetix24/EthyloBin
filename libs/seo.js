81625119c7b8e39cb725d36657c10eec180a9abf
Jetix24 <josepabloaguirre02@gmail.com>
2024-07-20 20:09:01 -0600

    Fix: image landing

--- libs/seo.js
+++ libs/seo.js
@@ -81,7 +81,7 @@ export const renderSchemaTags = () => {
           "@type": "SoftwareApplication",
           name: config.appName,
           description: config.appDescription,
-          image: `https://${config.domainName}/icon.webp`,
+          image: `https://${config.domainName}/icon.png`,
           url: `https://${config.domainName}/`,
           author: [
             {
