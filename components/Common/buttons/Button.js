6177e765c4aac9b9cd4356a5abbe0b1d5b3386d7
Jetix24 <josepabloaguirre02@gmail.com>
2024-08-14 21:58:15 -0600

    Refactor: others  ts a js

--- components/Common/buttons/Button.js
+++ components/Common/buttons/Button.js
@@ -2,17 +2,7 @@
 import React from 'react';
 import clsx from 'clsx';
 
-interface ButtonProps {
-  type?: 'button' | 'submit' | 'reset' | undefined;
-  fullWidth?: boolean;
-  children?: React.ReactNode;
-  onClick?: () => void;
-  secondary?: boolean;
-  danger?: boolean;
-  disabled?: boolean;
-}
-
-const Button: React.FC<ButtonProps> = ({
+const Button = ({
   type,
   fullWidth,
   children,
--- components/Input.js
+++ components/Input.js
@@ -1,23 +1,9 @@
 'use client';
 import React from 'react';
 import clsx from 'clsx';
-import {
-  FieldErrors,
-  FieldValues,
-  UseFormRegister
-} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
-interface InputProps {
-  label: string;
-  id: string;
-  type?: string;
-  required?: boolean;
-  register: UseFormRegister<FieldValues>,
-  errors: FieldErrors,
-  disabled?: boolean;
-}
-
-const Input: React.FC<InputProps> = ({
+const Input = ({
   label,
   id,
   type,
